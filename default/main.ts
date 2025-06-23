4162f24ab4c1a823cec96ab504db756572cf2c50
passapol <phasphlsuthahthrrm976@gmail.com>
2025-06-23 15:23:41 +0700

    setup

---
+++ default/main.ts
@@ -0,0 +1,5 @@
+import { myName, myCS, myFrind } from "./src/profile";
+
+console.log("ชื่อ \t\t :", myName);
+console.log("เหตุผลในการเรียน :", myCS);
+console.log("เพื่อนสนิท \t :", myFrind.join(', '));
---
+++ default/src/profile.ts
@@ -0,0 +1,3 @@
+export const myName = "พัสพล สุทาธรรม (Passapol sutatam)"
+export const myCS = "หวังทำงานสายนี้ เพราะว่าเงินมันดีและสนุก/ชอบเขียนโค้ด"
+export const myFrind = ["ไนท์","มอส","นาย","เล็ก","ภูมิ"]
